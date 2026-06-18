69f904145dfdeb2520c1a8e53f0e4efaed02277a
9sub <guseob0701@naver.com>
2026-06-18 18:32:09 +0900

    fix: user_id null 허용으로 수정

--- data/db/schema.sql
+++ data/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     nickname VARCHAR(50) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
